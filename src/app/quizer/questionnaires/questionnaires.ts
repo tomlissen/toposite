c773d54c2016ae5ebe66090719e23242930e5bd4
Tommy Delissen <tommy.delissen@ndw.nu>
2023-12-17 16:38:38 +0100

    Added groningen

--- src/app/quizer/questionnaires/questionnaires.ts
+++ src/app/quizer/questionnaires/questionnaires.ts
@@ -5,7 +5,8 @@ import {nl_provinces_and_capitals} from "./3_nl_provinces_and_capitals";
 import {nl_zeeen_en_meren} from "./4_nl_zeeen_en_meren";
 import {nl_rivieren_en_kanalen} from "./5_nl_rivieren_en_kanalen"
 import {nl_wateren} from "./6_nl_wateren"
-import {nl_friesland} from "./7_nl_friesland";
+import {nl_friesland} from "./7_nl_friesland"
+import {nl_groningen} from "./8_nl_groningen";
 
 export const questionnaires: QuizerQuestionnaireDTO[] = [
   nl_provinces,
@@ -14,5 +15,6 @@ export const questionnaires: QuizerQuestionnaireDTO[] = [
   nl_zeeen_en_meren,
   nl_rivieren_en_kanalen,
   nl_wateren,
-  nl_friesland
+  nl_friesland,
+  nl_groningen
 ];
